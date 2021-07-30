dbfeb7d70f698d6500fbd1e2d8b97d918606dd98
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2021-07-30 17:33:56 -0300

    Atividade Loja de Games com Spring Boot

--- Spring Boot/lojaGames/src/main/java/com/generation/lojaGames/model/Categoria.java
+++ Spring Boot/lojaGames/src/main/java/com/generation/lojaGames/model/Categoria.java
@@ -1,4 +1,4 @@
-package org.generation.blogPessoal.model;
+package com.generation.lojaGames.model;
 
 import java.util.List;
 
@@ -9,45 +9,62 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table (name = "tema")
-public class Tema {
-	
+@Table (name = "tb_categoria")
+
+public class Categoria {
+
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
-	//@Size (min= 10 , max= 100) pq???
+	@NotBlank
+	@Size (min = 5, max = 50)
+	private String tipo;
+	
+	@NotBlank
+	@Size (min = 5, max = 250)
 	private String descricao;
 	
-	@OneToMany (mappedBy = "tema", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties ("tema")
-	private List <Postagem> postagem;
-	
-	
+	@OneToMany (mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ("categoria")
+	private List <Produto> produto;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public List<Postagem> getPostagem() {
-		return postagem;
+
+	public List<Produto> getProdutos() {
+		return produto;
 	}
-	public void setPostagem(List<Postagem> postagem) {
-		this.postagem = postagem;
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produto = produtos;
 	}
 	
 	
